183e562ca1653b7f1203eab61a04dbf3d626462b
asn6878 <asn6878@gmail.com>
2025-01-07 05:20:32 +0900

    ♻️ refactor: di container 사용 시 사용할 심볼 선언

---
+++ feed-crawler/src/types/dependency-symbols.ts
@@ -0,0 +1,5 @@
+export const DEPENDENCY_SYMBOLS = {
+    DatabaseConnection: Symbol.for("DatabaseConnection"),
+    RssRepository: Symbol.for("RssRepository"),
+    FeedRepository: Symbol.for("FeedRepository"),
+}
