e1da3e15e52fba10bc53272cf10f17cc1d5fe345
asn6878 <asn6878@gmail.com>
2025-01-07 05:22:03 +0900

    ♻️ refactor: feedCrawler.ts 에 변경사항 적용 및 메인 함수명 변경

--- feed-crawler/src/main.ts
+++ feed-crawler/src/main.ts
@@ -1,16 +1,28 @@
 import logger from "./common/logger.js";
-import { feedCrawler } from "./feed-crawler.js";
-import { mysqlConnection } from "./common/mysql-access.js";
+import { FeedCrawler } from "./feed-crawler.js";
+import { container } from "./container";
+import {RssRepository} from "./repository/rss.repository";
+import {FeedRepository} from "./repository/feed.repository";
+import {DEPENDENCY_SYMBOLS} from "./types/dependency-symbols";
+import {DatabaseConnection} from "./types/database-connection";
 
-async function runCrawler() {
+async function main() {
   logger.info("==========작업 시작==========");
   const startTime = Date.now();
+
+  const rssRepository = container.resolve<RssRepository>("RssRepository");
+  const feedRepository = container.resolve<FeedRepository>("FeedRepository");
+  const dbConnection = container.resolve<DatabaseConnection>(DEPENDENCY_SYMBOLS.DatabaseConnection);
+
+  const feedCrawler = new FeedCrawler(rssRepository, feedRepository);
   await feedCrawler.start();
+
   const endTime = Date.now();
   const executionTime = endTime - startTime;
-  await mysqlConnection.end();
+
+  await dbConnection.end();
   logger.info(`실행 시간: ${executionTime / 1000}seconds`);
   logger.info("==========작업 완료==========");
 }
 
-runCrawler();
+main();
