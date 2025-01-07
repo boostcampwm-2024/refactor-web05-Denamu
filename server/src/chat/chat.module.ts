7df87c1469b8d5022edb9714ddf78c53102bd12f
codeVac513 <xssdsw@naver.com>
2025-01-07 17:27:47 +0900

    ♻️ refactor: chatService로 로직 분리에 따라 ChatModule에 의존성 주입 코드 작성

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ScheduleModule } from '@nestjs/schedule';
+import { ChatService } from './chat.service';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  providers: [ChatGateway],
+  providers: [ChatGateway, ChatService],
 })
 export class ChatModule {}
