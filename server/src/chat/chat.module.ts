9aa16ae9420cb1270e5a0886b656a52ad181d08f
codeVac513 <xssdsw@naver.com>
2025-01-08 17:48:39 +0900

    ♻️ refactor: module에 ChatSchedular 의존성 추가

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ChatService } from './chat.service';
+import { ChatScheduler } from './chat.scheduler';
 
 @Module({
   imports: [ScheduleModule.forRoot()],
-  providers: [ChatGateway, ChatService],
+  providers: [ChatGateway, ChatService, ChatScheduler],
 })
 export class ChatModule {}
