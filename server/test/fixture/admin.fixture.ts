f1c3c7070bf138dc92fa5bf1dac1d3d31fcec937
J_Coder <seok3765@naver.com>
2025-01-13 23:52:17 +0900

    ✅ test: admin 테스트 픽스처 암호화 추가

--- server/test/fixture/admin.fixture.ts
+++ server/test/fixture/admin.fixture.ts
@@ -6,13 +6,18 @@ export class AdminFixture {
     loginId: 'test1234',
     password: 'test1234!',
   };
-  static async createAdminFixture(
-    overwrites: Partial<Admin> = {},
-  ): Promise<Admin> {
+  static async createAdminCryptFixture(overwrites: Partial<Admin> = {}) {
     const admin = new Admin();
     Object.assign(admin, this.GENERAL_ADMIN);
     Object.assign(admin, overwrites);
     admin.password = await bcrypt.hash(admin.password, 10);
+    return admin;
+  }
+
+  static createAdminFixture(overwrites: Partial<Admin> = {}) {
+    const admin = new Admin();
+    Object.assign(admin, this.GENERAL_ADMIN);
+    Object.assign(admin, overwrites);
     return admin;
   }
 }
