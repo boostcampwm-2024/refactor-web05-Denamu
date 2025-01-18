a0990c6b11049722e2d53aea92b5cd5929a7a467
codeVac513 <xssdsw@naver.com>
2025-01-19 02:00:29 +0900

    📦 chore: test를 위한 TIME_INTERVAL 값 추가 및 constant의 TIME_INTERVAL 변수명 INTERVAL로 수정 후 test와 production 환경 변수 구분

--- feed-crawler/src/common/constant.ts
+++ feed-crawler/src/common/constant.ts
@@ -3,3 +3,7 @@ export const redisConstant = {
   FEED_RECENT_ALL_KEY: "feed:recent:*",
 };
 export const ONE_MINUTE = 60 * 1000;
+export const INTERVAL =
+  process.env.NODE_ENV !== "test"
+    ? parseInt(process.env.TIME_INTERVAL)
+    : parseInt(process.env.TEST_TIME_INTERVAL);
