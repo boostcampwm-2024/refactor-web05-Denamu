2197927e4e0684e767fd52951671342005a41e80
junyeokk <cjh4302@gmail.com>
2025-01-23 19:23:58 +0900

    ✅ test: chart skeleton 컴포넌트 테스트 코드 추가

--- client/src/components/chart/ChartSkeleton.tsx
+++ client/src/components/chart/ChartSkeleton.tsx
@@ -3,12 +3,12 @@ import PieChartItem from "@/components/chart/PieChartItem";
 
 export default function ChartSkeleton() {
   return (
-    <div className="p-8">
-      <div className="flex">
+    <div className="p-8" data-testid="chart-skeleton-container">
+      <div className="flex" data-testid="bar-charts-container">
         <BarChartItem title="전체 조회수" description="전체 조회수 TOP5" data={[]} color={true} />
         <BarChartItem title="오늘의 조회수" description="금일 조회수 TOP5" data={[]} color={false} />
       </div>
-      <div>
+      <div data-testid="pie-chart-container">
         <PieChartItem data={[]} title="플랫폼별 블로그 수" />
       </div>
     </div>
