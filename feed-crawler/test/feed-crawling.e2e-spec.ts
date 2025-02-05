3d3646a2ae4ae0ee8d7ca1da682bdda75178e708
J_Coder <seok3765@naver.com>
2025-02-06 02:14:54 +0900

    ✅ test: feed crawler 데이터 Mocking 방식으로 처리

--- feed-crawler/test/feed-crawling.e2e-spec.ts
+++ feed-crawler/test/feed-crawling.e2e-spec.ts
@@ -5,25 +5,43 @@ import { redisConstant } from "../src/common/constant";
 describe("feed crawling e2e-test", () => {
   const testContext = setupTestContainer();
   let feedCrawler: FeedCrawler;
+
   beforeAll(async () => {
     feedCrawler = new FeedCrawler(
       testContext.rssRepository,
       testContext.feedRepository,
+      testContext.rssParser
     );
   });
 
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it("RSS 피드가 정상적으로 DB, Redis에 저장된다.", async () => {
+    jest.spyOn(feedCrawler as any, "fetchRss").mockResolvedValue([
+      {
+        title: "Mock Title",
+        link: "https://example.com/mock",
+        pubDate: "Tue, 06 Feb 2024 12:00:00 GMT",
+      },
+    ]);
+
+    jest
+      .spyOn((feedCrawler as any).rssParser, "getThumbnailUrl")
+      .mockResolvedValue("https://example.com/mock/thumbnail");
+
     // given
     await testContext.dbConnection.executeQuery(
       `INSERT INTO rss_accept (name, user_name, email, rss_url, blog_platform) 
        VALUES (?, ?, ?, ?, ?)`,
       [
-        "나무보다 숲을",
-        "채준혁",
+        "test blog name",
+        "test user name",
         "test@test.com",
-        "https://laurent.tistory.com/rss",
-        "tistory",
-      ],
+        "https://test.com/rss",
+        "etc",
+      ]
     );
     // when
     await feedCrawler.start();
@@ -31,15 +49,16 @@ describe("feed crawling e2e-test", () => {
     // then
     const feedsFromDB = await testContext.dbConnection.executeQuery(
       "SELECT * FROM feed",
-      [],
+      []
     );
+
     const recentFeedsKeys = [];
     let cursor = "0";
     do {
       const [newCursor, keys] = await testContext.redisConnection.scan(
         cursor,
         redisConstant.FEED_RECENT_ALL_KEY,
-        100,
+        100
       );
       recentFeedsKeys.push(...keys);
       cursor = newCursor;
@@ -59,7 +78,7 @@ describe("feed crawling e2e-test", () => {
         "test@test.com",
         "https://test.tistory.com/test",
         "tistory",
-      ],
+      ]
     );
     // when
     await feedCrawler.start();
@@ -67,7 +86,7 @@ describe("feed crawling e2e-test", () => {
     // then
     const feeds = await testContext.dbConnection.executeQuery(
       "SELECT * FROM feed",
-      [],
+      []
     );
     expect(feeds.length).toBe(0);
   });
