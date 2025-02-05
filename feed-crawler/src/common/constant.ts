33da3368a2a13e57a6c450ae35f0ade844e5c728
J_Coder <seok3765@naver.com>
2025-02-06 00:44:23 +0900

    ♻️ refactor: 환경 변수 이름, 환경 변수 위치, 로깅 방식 Server Feed Crawler 통일

--- feed-crawler/src/common/constant.ts
+++ feed-crawler/src/common/constant.ts
@@ -1,14 +1,9 @@
-import * as dotenv from "dotenv";
-
-dotenv.config({
-  path: process.env.NODE_ENV === "production" ? "feed-crawler/.env" : ".env",
-});
 export const CONNECTION_LIMIT = 50;
 export const redisConstant = {
   FEED_RECENT_ALL_KEY: "feed:recent:*",
 };
 export const ONE_MINUTE = 60 * 1000;
-export const INTERVAL =
-  process.env.NODE_ENV === "test"
-    ? parseInt(process.env.TEST_TIME_INTERVAL)
-    : parseInt(process.env.TIME_INTERVAL);
+export const TIME_INTERVAL =
+  process.env.NODE_ENV !== "test"
+    ? parseInt(process.env.TIME_INTERVAL)
+    : Number.MAX_SAFE_INTEGER;
