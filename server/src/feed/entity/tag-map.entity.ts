76e79af194f6f5ede8ed2696612e3c534c779262
J_Coder <seok3765@naver.com>
2025-02-12 02:25:06 +0900

    ♻️ refactor: 태그 맵핑 테이블 엔티티 속성 추가

--- server/src/feed/entity/tag-map.entity.ts
+++ server/src/feed/entity/tag-map.entity.ts
@@ -9,14 +9,8 @@ import { Feed } from './feed.entity';
 
 @Entity({ name: 'tag_map' })
 export class TagMap extends BaseEntity {
-  @PrimaryColumn({ name: 'feed_id', type: 'number' })
-  @ManyToOne(() => Feed, (feed) => feed.tag, {
-    nullable: false,
-    onUpdate: 'CASCADE',
-    onDelete: 'CASCADE',
-  })
-  @JoinColumn({ name: 'feed_id' })
-  feed: Feed;
+  @PrimaryColumn({ name: 'feed_id' })
+  feedId: number;
 
   @PrimaryColumn({
     name: 'tag',
@@ -24,4 +18,12 @@ export class TagMap extends BaseEntity {
     nullable: false,
   })
   tag: string;
+
+  @ManyToOne(() => Feed, (feed) => feed.tag, {
+    nullable: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'feed_id' })
+  feed: Feed;
 }
