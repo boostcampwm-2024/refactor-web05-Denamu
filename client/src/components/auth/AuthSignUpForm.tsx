274ef50f4b9a33623b7748f6e31853f54652f122
junyeokk <cjh4302@gmail.com>
2025-02-13 10:29:27 +0900

    ✨ feat: 회원가입 폼 구현

---
+++ client/src/components/auth/AuthSignUpForm.tsx
@@ -0,0 +1,37 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+import { AuthCard } from "@/components/auth/AuthCard.tsx";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export const AuthSignUpForm = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <>
+      <AuthCard title="회원가입" description="회원가입을 해주세요">
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Input type="email" placeholder="이메일을 입력하세요" required />
+            <Input type="password" placeholder="비밀번호를 입력하세요" required />
+            <Input type="text" placeholder="이름을 입력해주세요" required />
+            <Input type="text" placeholder="닉네임을 입력해주세요" required />
+          </div>
+          <Button className="w-full" type="submit">
+            회원가입
+          </Button>
+        </form>
+        <div className="mt-4">
+          <Button
+            variant="link"
+            className="text-muted-foreground underline underline-offset-4 h-auto p-0"
+            onClick={() => navigate("/signin", { state: { from: location.pathname } })}
+          >
+            이미 계정이 있으신가요?
+          </Button>
+        </div>
+      </AuthCard>
+    </>
+  );
+};
