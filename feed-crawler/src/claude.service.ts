811784baf652411a15017e59258bb0ed42ff209c
CodeVac513 <xssdsw@naver.com>
2025-02-18 15:51:54 +0900

    ♻️ refactor: promise 판별 로직 단순화, 에러 로깅 추가

--- feed-crawler/src/claude.service.ts
+++ feed-crawler/src/claude.service.ts
@@ -12,7 +12,7 @@ export class ClaudeService {
 
   constructor(
     private readonly tagMapRepository: TagMapRepository,
-    private readonly feedRepository: FeedRepository,
+    private readonly feedRepository: FeedRepository
   ) {
     this.client = new Anthropic({
       apiKey: process.env.AI_API_KEY,
@@ -22,97 +22,60 @@ export class ClaudeService {
   async useCaludeService(feeds: FeedDetail[]) {
     const processedFeeds = await Promise.allSettled(
       feeds.map(async (feed) => {
-        try {
-          const params: Anthropic.MessageCreateParams = {
-            max_tokens: 8192,
-            system: PROMPT_CONTENT,
-            messages: [{ role: "user", content: feed.content }],
-            model: "claude-3-5-haiku-latest",
-          };
-          const message = await this.client.messages.create(params);
-          let responseText: string = message.content[0]["text"];
-          responseText = responseText.replace(/\n/g, "");
-          const result: ClaudeResponse = JSON.parse(responseText);
+        const params: Anthropic.MessageCreateParams = {
+          max_tokens: 8192,
+          system: PROMPT_CONTENT,
+          messages: [{ role: "user", content: feed.content }],
+          model: "claude-3-5-haiku-latest",
+        };
+        const message = await this.client.messages.create(params);
+        let responseText: string = message.content[0]["text"];
+        responseText = responseText.replace(/\n/g, "");
+        const result: ClaudeResponse = JSON.parse(responseText);
 
-          await Promise.all([
-            this.generateTag(feed, result["tags"]),
-            this.summarize(feed, result["summary"]),
-          ]);
-          return {
-            succeeded: true,
-            feed,
-          };
-        } catch (error) {
-          logger.error(
-            `${feed.id}의 태그 생성, 컨텐츠 요약 에러 발생: `,
-            error,
-          );
-          return {
-            succeeded: false,
-            feed,
-          };
-        }
-      }),
+        await Promise.all([
+          this.generateTag(feed, result["tags"]),
+          this.summarize(feed, result["summary"]),
+        ]);
+
+        return feed;
+      })
     );
 
-    // TODO: Refactor
     const successFeeds = processedFeeds
-      .map((result) =>
-        result.status === "fulfilled" && result.value.succeeded === true
-          ? result.value.feed
-          : null,
-      )
-      .filter((result) => result !== null);
+      .filter((result) => result.status === "fulfilled")
+      .map((result) => result.value);
 
-    // TODO: Refactor
     const failedFeeds = processedFeeds
       .map((result, index) => {
         if (result.status === "rejected") {
-          const failedFeed = feeds[index];
-          return {
-            succeeded: false,
-            feed: failedFeed,
-          };
+          logger.error(
+            `[ClaudeService] 피드 분석 도중 에러가 발생했습니다.
+            에러 메시지: ${result.reason.message}
+            스택 트레이스: ${result.reason.stack}`
+          );
+          return feeds[index];
         }
-        return result.status === "fulfilled" && result.value.succeeded === false
-          ? result.value
-          : null;
+        return null;
       })
-      .filter((result) => result !== null && result.succeeded === false)
-      .map((result) => result.feed);
+      .filter((result) => result !== null);
 
     logger.info(
-      `${successFeeds.length}개의 태그 생성 및 컨텐츠 요약이 성공했습니다.\n ${failedFeeds.length}개의 태그 생성 및 컨텐츠 요약이 실패했습니다.`,
+      `${successFeeds.length}개의 태그 생성 및 컨텐츠 요약이 성공했습니다.\n ${failedFeeds.length}개의 태그 생성 및 컨텐츠 요약이 실패했습니다.`
     );
 
     return [...successFeeds, ...failedFeeds];
   }
 
   private async generateTag(feed: FeedDetail, tags: Record<string, number>) {
-    try {
-      const tagList = Object.keys(tags);
-      if (tagList.length === 0) return;
-      await this.tagMapRepository.insertTags(feed.id, tagList);
-      feed.tag = tagList;
-    } catch (error) {
-      logger.error(
-        `[DB] 태그 데이터를 저장하는 도중 에러가 발생했습니다.
-      에러 메시지: ${error.message}
-      스택 트레이스: ${error.stack}`,
-      );
-    }
+    const tagList = Object.keys(tags);
+    if (tagList.length === 0) return;
+    await this.tagMapRepository.insertTags(feed.id, tagList);
+    feed.tag = tagList;
   }
 
   private async summarize(feed: FeedDetail, summary: string) {
-    try {
-      await this.feedRepository.insertSummary(feed.id, summary);
-      feed.summary = summary;
-    } catch (error) {
-      logger.error(
-        `[DB] 게시글 요약 데이터를 저장하는 도중 에러가 발생했습니다.
-      에러 메시지: ${error.message}
-      스택 트레이스: ${error.stack}`,
-      );
-    }
+    await this.feedRepository.insertSummary(feed.id, summary);
+    feed.summary = summary;
   }
 }
