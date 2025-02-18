a4cbf0f23f3fd364edc802cb8b868314034d31dc
J_Coder <seok3765@naver.com>
2025-02-18 22:03:09 +0900

    ♻️ refactor: 1분마다 AI 요청을 보내어 rate limit 해결 스케쥴러 추가

--- feed-crawler/src/main.ts
+++ feed-crawler/src/main.ts
@@ -7,28 +7,37 @@ import { FeedRepository } from "./repository/feed.repository";
 import { DEPENDENCY_SYMBOLS } from "./types/dependency-symbols";
 import { DatabaseConnection } from "./types/database-connection";
 import { ClaudeService } from "./claude.service";
+import * as schedule from "node-schedule";
+import { RedisConnection } from "./common/redis-access";
+import { TagMapRepository } from "./repository/tag-map.repository";
 
 async function main() {
   logger.info("==========작업 시작==========");
   const startTime = Date.now();
 
   const rssRepository = container.resolve<RssRepository>(
-    DEPENDENCY_SYMBOLS.RssRepository,
+    DEPENDENCY_SYMBOLS.RssRepository
   );
   const feedRepository = container.resolve<FeedRepository>(
-    DEPENDENCY_SYMBOLS.FeedRepository,
+    DEPENDENCY_SYMBOLS.FeedRepository
+  );
+  const tagMapRepository = container.resolve<TagMapRepository>(
+    DEPENDENCY_SYMBOLS.TagMapRepository
   );
   const dbConnection = container.resolve<DatabaseConnection>(
-    DEPENDENCY_SYMBOLS.DatabaseConnection,
+    DEPENDENCY_SYMBOLS.DatabaseConnection
   );
   const claudeService = container.resolve<ClaudeService>(
-    DEPENDENCY_SYMBOLS.ClaudeService,
+    DEPENDENCY_SYMBOLS.ClaudeService
+  );
+  const redisConnection = container.resolve<RedisConnection>(
+    DEPENDENCY_SYMBOLS.RedisConnection
   );
 
   const feedCrawler = new FeedCrawler(
     rssRepository,
     feedRepository,
-    claudeService,
+    claudeService
   );
   await feedCrawler.start();
 
@@ -38,6 +47,19 @@ async function main() {
   await dbConnection.end();
   logger.info(`실행 시간: ${executionTime / 1000}seconds`);
   logger.info("==========작업 완료==========");
+
+  schedule.scheduleJob(
+    "AI API PER MINUTE REQUEST RATE LIMIT",
+    "*/1 * * * *",
+    () => {
+      const aiRequest = new ClaudeService(
+        tagMapRepository,
+        feedRepository,
+        redisConnection
+      );
+      aiRequest.startRequestAI();
+    }
+  );
 }
 
 main();
