410f45e6c6afe1e17f39835fcae9be453a8e5aa5
junyeokk <cjh4302@gmail.com>
2025-02-21 11:23:40 +0900

    ♻️ refactor: dayCell tooltip hover 시 duration 200ms로 변경 및 UX 향상

--- client/src/components/profile/header/ui/ActivityGraph/DayCell.tsx
+++ client/src/components/profile/header/ui/ActivityGraph/DayCell.tsx
@@ -5,7 +5,7 @@ import { getColorClass } from "@/utils/color.ts";
 import { DayInfo } from "@/types/activity.ts";
 
 export const DayCell = ({ dayInfo }: { dayInfo: DayInfo }) => (
-  <Tooltip>
+  <Tooltip delayDuration={200}>
     <TooltipTrigger>
       <div className={`w-2.5 h-2.5 rounded-sm ${getColorClass(dayInfo.count)}`} />
     </TooltipTrigger>
